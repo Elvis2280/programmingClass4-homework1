7f00bf1c07ae5a1cda2d15e9e6530d8312c16631
Elvis Miranda <elvismiranda924g@gmail.com>
2021-10-27 02:40:33 -0500

    tarea3 added

--- tarea3/tarea3.py
+++ tarea3/tarea3.py
@@ -1,80 +1,40 @@
-import sqlalchemy
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import session
-# Elvis Miranda 4-803-2349
+import pymongo
+from pymongo import MongoClient
 
-# lo usamos para conectarnos a la database con sqlalchemy
-# debemos pasarle los datos como password, username port etc de nuestra configuracion en mariadb
-engine = sqlalchemy.create_engine(
-    "mariadb+mariadbconnector://root:2280@127.0.0.1:3306/slang")
+cluster = MongoClient(
+    "mongodb+srv://Tarea3:Programacion@cluster0.iomhm.mongodb.net/TareaUip?retryWrites=true&w=majority")
 
-Base = declarative_base()
-
-# creamos la tabla y los valores de la misma en este caso tenemos id, palabra y definicion como columnas
-
-
-class slang(Base):
-    __tablename__ = 'diccionario'
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    palabra = sqlalchemy.Column(sqlalchemy.Text, unique=True)
-    definicion = sqlalchemy.Column(sqlalchemy.Text)
-
-# verifica si la palabra existe en la base de datos
+db = cluster["TareaUip"]
+collection = db["Slang"]
 
 
 def checkExistPalabra(palabra):
-    check = session.query(session.query(slang).filter(
-        slang.palabra == palabra).exists()).scalar()
-    return check
-
-# añadimos una nueva palabra a la base de datos
-
-
-def addPalabraDef(palabra, definicion):
-    newPalabra = slang(palabra=palabra,
-                       definicion=definicion)
-    session.add(newPalabra)
-    session.commit()
-    print("\n palabra agregada correctamente!")
-
-# actualiza una palabra existente
+    check = collection.find_one(
+        {"palabra": palabra})
+    if(check == None):
+        return False
+    else:
+        return True
 
 
 def updatePalabra(oldPalabra, newPalabra, newDefinicion):
-    select = session.query(slang).filter(
-        slang.palabra == oldPalabra).one()
-    select.palabra = newPalabra
-    select.definicion = newDefinicion
-    session.commit()
-    print("\n La palabra " + oldPalabra + " fue actualizada!")
-
-# borra una palabra existente
+    collection.update_one({"palabra": oldPalabra}, {"$set": {
+        "palabra": newPalabra,
+        "definicion": newDefinicion
+    }})
 
 
 def deletePalabra(palabra):
-    session.query(session.query(slang).filter(
-                  slang.palabra == palabra).delete())
-    session.commit()
-    print("\n Palabra eliminada!")
-
-# mostrar los valores de la columna palabra
+    collection.delete_one({"palabra": palabra})
 
 
 def showAllPalabras():
-    palabras = session.query(slang).all()
-    print("\n Lista de palabras \n")
+    palabras = collection.find()
     i = 0
     for row in palabras:
         i += 1
-        print(f'{i}. {row.palabra}')
-
-
-Base.metadata.create_all(engine)
-
-# crea la sesion para comunicarse con mariadb a nuestra database
-Session = sqlalchemy.orm.sessionmaker()
-Session.configure(bind=engine)
-session = Session()
+        print(
+            f'{i}. Palabra: {row["palabra"]} Definicion: {row["definicion"]}')
 
 
 while True:
@@ -90,10 +50,14 @@ while True:
         inputDefinicion = input(
             "\n por ultimo ingrese la definicion de la palabra \n")
         if(len(inputPalabra) and len(inputDefinicion)):
+
             if(checkExistPalabra(inputPalabra)):
                 print("\n Esta palabra ya existe por favor de agregar otra")
             else:
-                addPalabraDef(inputPalabra, inputDefinicion)
+                collection.insert_one({
+                    "palabra": inputPalabra,
+                    "definicion": inputDefinicion
+                })
         else:
             print("\n Por favor llenar ambos campos de informacion")
 
@@ -134,9 +98,9 @@ while True:
             "\n Ingrese la palabra que desea ver su significado \n")
         if(len(inputPalabra)):
             if(checkExistPalabra(inputPalabra)):
-                palabra = session.query(slang).filter(
-                    slang.palabra == inputPalabra).scalar()
-                print(f'La definicion es: {palabra.definicion}')
+                getPalabra = collection.find_one(
+                    {"palabra": inputPalabra})
+                print(f'La definicion es: {getPalabra["definicion"]}')
             else:
                 print("\n La palabra no existe!")
 
